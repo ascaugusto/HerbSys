09ec6a3091809abaeaf03b48b390718d0b64ff9e
Augusto Coelho <ascaugusto@gmail.com>
2017-06-30 09:12:03 -0300

    commit telas

---
+++ HerbSys/HerbSys/View/FormRegistraVenda.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace HerbSys.View
+{
+    partial class FormRegistraVenda
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.add_button = new System.Windows.Forms.Button();
+            this.cbClientes = new System.Windows.Forms.ComboBox();
+            this.cliente1 = new System.Windows.Forms.Label();
+            this.cbProdutos = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbProdutos = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // add_button
+            // 
+            this.add_button.AccessibleName = "";
+            this.add_button.Location = new System.Drawing.Point(153, 89);
+            this.add_button.Name = "add_button";
+            this.add_button.Size = new System.Drawing.Size(27, 23);
+            this.add_button.TabIndex = 0;
+            this.add_button.Text = "+";
+            this.add_button.UseVisualStyleBackColor = true;
+            // 
+            // cbClientes
+            // 
+            this.cbClientes.FormattingEnabled = true;
+            this.cbClientes.Location = new System.Drawing.Point(26, 41);
+            this.cbClientes.Name = "cbClientes";
+            this.cbClientes.Size = new System.Drawing.Size(267, 21);
+            this.cbClientes.TabIndex = 1;
+            this.cbClientes.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // cliente1
+            // 
+            this.cliente1.AutoSize = true;
+            this.cliente1.Location = new System.Drawing.Point(23, 25);
+            this.cliente1.Name = "cliente1";
+            this.cliente1.Size = new System.Drawing.Size(92, 13);
+            this.cliente1.TabIndex = 2;
+            this.cliente1.Text = "Escolha o Cliente:";
+            this.cliente1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // cbProdutos
+            // 
+            this.cbProdutos.FormattingEnabled = true;
+            this.cbProdutos.Location = new System.Drawing.Point(26, 89);
+            this.cbProdutos.Name = "cbProdutos";
+            this.cbProdutos.Size = new System.Drawing.Size(121, 21);
+            this.cbProdutos.TabIndex = 3;
+            this.cbProdutos.SelectedIndexChanged += new System.EventHandler(this.cbProdutos_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Escolha os produtos:";
+            // 
+            // lbProdutos
+            // 
+            this.lbProdutos.FormattingEnabled = true;
+            this.lbProdutos.Location = new System.Drawing.Point(27, 137);
+            this.lbProdutos.Name = "lbProdutos";
+            this.lbProdutos.Size = new System.Drawing.Size(266, 121);
+            this.lbProdutos.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(311, 213);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Valor Total:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(314, 238);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(58, 20);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 281);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(267, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Registrar Venda";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FormRegistraVenda
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 316);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbProdutos);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbProdutos);
+            this.Controls.Add(this.cliente1);
+            this.Controls.Add(this.cbClientes);
+            this.Controls.Add(this.add_button);
+            this.Name = "FormRegistraVenda";
+            this.Text = "FormRegistraVenda";
+            this.Load += new System.EventHandler(this.FormRegistraVenda_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button add_button;
+        private System.Windows.Forms.ComboBox cbClientes;
+        private System.Windows.Forms.Label cliente1;
+        private System.Windows.Forms.ComboBox cbProdutos;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lbProdutos;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ HerbSys/HerbSys/View/FormRegistraVenda.cs
@@ -0,0 +1,59 @@
+﻿using HerbSys.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HerbSys.View
+{
+    public partial class FormRegistraVenda : Form
+    {
+        public FormRegistraVenda()
+        {
+            InitializeComponent();
+        }
+
+        private void FormRegistraVenda_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cbClientes.Text = "09902630618";
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            VendasBD.DadosVenda dados = new VendasBD.DadosVenda();
+            dados.cpf_cnpj = cbClientes.Text;
+            dados.valor_total = cbProdutos.Text;
+
+        }
+
+        private void cbProdutos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cbProdutos.Text = "1";
+        }
+    }
+}
